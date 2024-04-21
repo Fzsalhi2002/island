82a8333f45409eefab7e676aecf2f082266f8500
Fzsalhi2002 <fatimaezzahrasalhi2002@gmail.com>
2024-04-21 20:11:06 +0200

    edit lastt

---
+++ Assets/Q.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Q : MonoBehaviour
+{
+    public TextMeshProUGUI feedbackText;
+    public TextMeshProUGUI retryText;
+
+    void ClearFeedbackText()
+    {
+        feedbackText.text = "";
+    }
+
+    private void OnMouseDown()
+    {
+        ClearFeedbackText();
+        feedbackText.text = "Wow! Score: 85";
+        retryText.gameObject.SetActive(false);
+
+        Invoke("LoadSceneWithDelay", 3f);
+    }
+
+    void LoadSceneWithDelay()
+    {
+        // Charger la nouvelle sc�ne "T2L1"
+        SceneManager.LoadScene("puzzle");
+    }
+}
