38af594881f01dfae6bbf66d74cfb36099226a32
Fzsalhi2002 <fatimaezzahrasalhi2002@gmail.com>
2024-04-23 10:17:26 +0200

    lastttttttt

--- Assets/Scripts/ControleurJoueur.cs
+++ Assets/Scripts/ControleurJoueur.cs
@@ -1,40 +1,54 @@
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
-public class ControleurJoueur : MonoBehaviour {
-public float vitesse;
-public Text countText;
-public Text winText;
-private Rigidbody rb;
-private int count;
-void Start ()
+using UnityEngine.SceneManagement; // Ajout de la référence au système de gestion de scène
+
+public class ControleurJoueur : MonoBehaviour
 {
-rb = GetComponent <Rigidbody> ();
-count =0;
-SetCountText ();
-winText.text = "";
-}
-void FixedUpdate ()
-{
-float moveHorizontal = Input.GetAxis ("Horizontal");
-float moveVertical = Input.GetAxis ("Vertical");
-Vector3 mouvement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-rb.AddForce (mouvement * vitesse);
-}
-void OnTriggerEnter (Collider other)
-{if (other.gameObject.CompareTag ( "Cible"))
-{
-other.gameObject.SetActive (false);
-count = count + 1;
-SetCountText ();
-}
-}
-void SetCountText ()
-{
-countText.text = "Count: " + count.ToString ();
-if (count >= 5)
-{
-winText.text = "Wow! Score:15";
-}
-}
+    public float vitesse;
+    public Text countText;
+    public Text winText;
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 mouvement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(mouvement * vitesse);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Cible"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 5)
+        {
+            winText.text = "Wow! Score: 15";
+            Invoke("LoadNextScene", 3f); // Appelle la méthode LoadNextScene après 3 secondes
+        }
+    }
+
+    void LoadNextScene()
+    {
+        // Charge la prochaine scène par son nom (remplacez "NomDeLaSceneSuivante" par le nom de votre scène)
+        SceneManager.LoadScene("DemoD");
+    }
 }
